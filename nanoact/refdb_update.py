431b58c2255f2b76dcc4ca58df8f81165b68a4b9
OU JIEHAO <allenstorm2005@gmail.com>
2023-06-26 21:19:21 +0800

    update db

--- nanoact/refdb_update.py
+++ nanoact/refdb_update.py
@@ -7,15 +7,15 @@ import gzip
 dumb = nanoact.NanoAct()
 libpath = os.path.dirname(nanoact.__file__)
 #%%
-gbff_list = [#"https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Fungi/fungi.ITS.gbff.gz",
-             #"https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Fungi/fungi.28SrRNA.gbff.gz",
+gbff_list = ["https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Fungi/fungi.ITS.gbff.gz",
+             "https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Fungi/fungi.28SrRNA.gbff.gz",
              "https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Fungi/fungi.18SrRNA.gbff.gz",
-             #"https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Bacteria/bacteria.16SrRNA.gbff.gz",
-             #"https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Bacteria/bacteria.23SrRNA.gbff.gz",
-             #"https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Bacteria/bacteria.5SrRNA.gbff.gz",
-             #"https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Archaea/archaea.16SrRNA.gbff.gz",
-             #"https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Archaea/archaea.23SrRNA.gbff.gz",
-             #"https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Archaea/archaea.5SrRNA.gbff.gz"
+             "https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Bacteria/bacteria.16SrRNA.gbff.gz",
+             "https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Bacteria/bacteria.23SrRNA.gbff.gz",
+             "https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Bacteria/bacteria.5SrRNA.gbff.gz",
+             "https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Archaea/archaea.16SrRNA.gbff.gz",
+             "https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Archaea/archaea.23SrRNA.gbff.gz",
+             "https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Archaea/archaea.5SrRNA.gbff.gz"
              ]
 
 refdb_path = f"{libpath}/refdb/"
@@ -36,8 +36,12 @@ for gbff_URI in gbff_list:
 for tax in os.listdir(refdb_path):
     if tax.endswith(".fas"):
         print("Compressing "+tax)
+        dumb._exec("gzip "+refdb_path+tax)
         with open(refdb_path+tax, "rb") as f_in:
             with gzip.open(refdb_path+tax+".gz", "wb") as f_out:
                 f_out.writelines(f_in)
+            f_out.close()
+        f_in.close()
+            
         os.remove(refdb_path+tax)
 # %%
